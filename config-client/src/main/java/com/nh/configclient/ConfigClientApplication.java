fd20b8c1c81558a543e2ce09b027afcdf2f75a64
netkeeper_ <xxx>
2019-05-30 10:37:12 +0800

    add

--- config-client/src/main/java/com/nh/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/nh/configclient/ConfigClientApplication.java
@@ -3,25 +3,29 @@ package com.nh.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableEurekaClient
+@RestController
+@EnableDiscoveryClient
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
-    @Value("${foo}")
-    String foo;
+    @Value("${version}")
+    String version;
 
     @RequestMapping(value = "/configTest")
     public String configTest(){
-        return foo;
+        return version;
     }
 }
