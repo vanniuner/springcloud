bcc70ffad7ad951f1a1843c1c736f2b5ea495485
netkeeper_ <xxx>
2019-05-30 14:58:14 +0800

    merge

--- config-client/src/main/java/com/nh/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/nh/configclient/ConfigClientApplication.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableDiscoveryClient
 @EnableEurekaClient
-@RefreshScope
+//@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
