71dfe49e899ca4b6c5d54249590af81e5789c166
netkeeper_ <xxx>
2019-05-31 09:44:20 +0800

    fix

--- breed-pig-service/src/main/java/com/nh/breedpigservice/Controller.java
+++ breed-pig-service/src/main/java/com/nh/breedpigservice/Controller.java
@@ -1,6 +1,7 @@
 package com.nh.breedpigservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,12 @@ public class Controller {
     @Autowired
     ServiceDemo serviceDemo;
 
+    @Value("${foo}")
+    String a;
+
     @RequestMapping(value = "consume")
     public String cons(){
-        return serviceDemo.consume();
+        return serviceDemo.consume() + a;
     }
 
 
