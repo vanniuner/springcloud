83a48e405e2d360db3228eb2725b9415f40487e4
vanniuner <461382141@qq.com>
2019-08-05 10:11:33 +0800

    add: kotlin compile

--- eureka-server/src/test/java/mykotlin/highlevelfun/Country.kt
+++ eureka-server/src/test/java/mykotlin/highlevelfun/Country.kt
@@ -1,11 +1,11 @@
 package mykotlin.highlevelfun
 
-data class Country(val name:String,val contry:String,val popul:Int)
+data class Country(val name:String,val country:String,val population:Int)
 
 class CountryApp{
-    fun filterCountries(contries:List<Country>,test:(Country) -> Boolean):List<Country>{
+    fun filterCountries(countryList:List<Country>,test:(Country) -> Boolean):List<Country>{
         val res = mutableListOf<Country>()
-        for(c in contries){
+        for(c in countryList){
             if(test(c)){
                 res.add(c)
             }
@@ -16,16 +16,17 @@ class CountryApp{
 
 class CountryTest {
     fun isBigEuropeanCountry(country: Country): Boolean {
-        return country.contry == "EU" && country.popul > 10000
+        return country.country == "EU" && country.population > 10000
     }
 }
 
 fun main(){
     val countryApp = CountryApp()
+    val countryTest:CountryTest = CountryTest()
     val list:List<Country> = listOf<Country>(Country("EUR","EU",20000),
             Country("EUR2","EU2",20000))
-    val listr:List<Country> = countryApp.filterCountries(list,{c->c.contry == "EU" && c.popul > 10000})
-    println(listr)
+    val listResult:List<Country> = countryApp.filterCountries(list,countryTest::isBigEuropeanCountry)
+    println(listResult)
 
     listOf(1, 2, 3).forEach { item -> println(item) }
 
